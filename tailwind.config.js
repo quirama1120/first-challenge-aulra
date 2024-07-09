fbdab04f8d9b2494972ea3f11feba6fb310240b4
Christian Quirama <quirama1120@gmail.com>
2024-07-09 01:43:40 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      screens: {
+        'tablet': '768px',
+        'laptop': '1024px',
+        'desktop': '1280px',
+      },
+      height: {
+        '70vh': '70vh',
+        '30vh': '30vh',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
